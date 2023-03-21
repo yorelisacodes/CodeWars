44d15d1e95e6f5f3bc4d10f19a68080539ccf9b5
Yorelisa Calcano <ycalcano@gmail.com>
2023-03-21 19:27:49 -0400

    added comments to my codewars

--- week6/answer1.js
+++ week6/answer1.js
@@ -2,3 +2,5 @@ function simpleMultiplication(number) {
   return number % 2 === 0 ? number * 8 : number * 9
  }
 
+// description
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
