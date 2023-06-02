064c3230ddbb3dbb5f917db586c9c4f41be13219
Yorelisa Calcano <ycalcano@gmail.com>
2023-06-02 11:25:57 -0400

    codewars completed

---
+++ answerthirteen/answer55.js
@@ -0,0 +1,23 @@
+// To square(root) or not to square(root)
+
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+function squareOrSquareRoot(array) {
+  let solution = []
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] % Math.sqrt(array[i]) === 0) {
+          solution.push(Math.sqrt(array[i]))
+      } else {
+          solution.push(array[i] * array[i])
+      }
+  } return solution
+}
